8cb854bad3c06d48b39ef63ed4bb3f9a0cb481a4
yizhi <yizhifang123@gmail.com>
2024-02-14 19:32:58 -0800

    finished crop harvesters

--- cropharvesters_Feb13.cpp
+++ cropharvesters_Feb13.cpp
@@ -7,13 +7,19 @@ using namespace std;
 int main() {
     int x1, y1, x2, y2, x3, y3, x4, y4;
     cin >> x1 >> y1 >> x2 >> y2 >> x3 >> y3 >> x4 >> y4;
-    
-    int smallest_x = min(min(x1, x2), min(x3, x4));
-    int biggest_x = max(max(x1, x2), max(x3, x4));
-    
-    int smallest_y = min(min(y1, y2), min(y3, y4));
-    int biggest_y = max(max(y1, y2), max(y3, y4));
-
-    cout << (biggest_y-smallest_y)*(biggest_x - smallest_x);
+    if((x3<=x1 & x4>=x2) & (y3<=y1 & y4>=y2)){
+        cout << 0;
+    }
+    else if((x3<=x1 & y3<=y1)&(x2>x4 & y2<y4)){
+        //POINT 1 is swallowed by p3 p4 , but p2 is outside     
+        cout << abs((y2-y1) * (x4-x2)); 
+    }
+    else if((x2<=x4 & y2<=y4) & (x3<x2 & y2>y3) & (y1 < y3 || y1>y4) & (x1>x3 & x1<x4)){
+        //point 2 is swallowed by p3 p4, but p1 is available(Up and down)
+        cout << abs((x2-x1) * (y3-y1));
+    }
+    else{
+        cout << (x2 - x1)*(y2-y1);
+    }
     return 0;
 }
